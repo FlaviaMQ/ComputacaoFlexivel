aa12f927b652c2f0be9a25924ce7b2909f9e63e5
Flavia <flmqueiroz@gmail.com>
2026-06-23 19:45:53 -0300

    mes-hora-temperatura

--- Perceptron_MultiplasCamadas.py
+++ Perceptron_MultiplasCamadas.py
@@ -6,9 +6,9 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 # 1. Ler a aba da planilha
-dados = pd.read_excel("Conforto-Termico-anapolis.xlsm", sheet_name="treino2")
+dados = pd.read_excel("Conforto-Termico-anapolis.xlsm", sheet_name="treino3")
 
-dados = dados.dropna(subset=["Mes", "Hora", "Classe"])
+dados = dados.dropna(subset=["Mes", "Hora", "Temperatura", "Classe"])
 
 # remove linhas totalmente vazias
 dados = dados.dropna(how="all")
@@ -35,7 +35,7 @@ print((pd.crosstab(dados["Mes"], dados["Classe"], normalize="index") * 100).roun
 
 
 # 2. Separar entradas e saída
-X = dados[["Mes", "Hora"]]
+X = dados[["Mes", "Hora", "Temperatura"]]
 y = dados["Classe"]
 
 # 3. Dividir dados: 70% treino, 15% validação, 15% teste
